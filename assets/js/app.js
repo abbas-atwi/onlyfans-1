e87709177b15b6c344fd49f226e800586eefb8b4
Abbas Atwi <abbas_9589_new@hotmail.com>
2023-07-08 00:46:19 -0300

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,14 @@
 const browser = window.matchMedia("(max-width: 450px)").matches;
 const moreInfo = document.querySelector(".bio-info a");
+
+const followSybscribe = document.querySelector(".follow-me a");
 moreInfo.addEventListener("click", (e) => {
   e.preventDefault();
   alert("jesus ta vendo !!!");
+});
+
+followSybscribe.addEventListener("click", () => {
+  alert("Captura com a camera frontal !! ");
 });
 const optionsFieldSet = document.querySelectorAll("fieldset");
 console.log(optionsFieldSet[1]);
